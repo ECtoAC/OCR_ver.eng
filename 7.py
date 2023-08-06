4950060015f15d4f972c6c9e43b7448b9a80708a
ACtoEC <shimpei3991@gmail.com>
2023-08-06 17:12:44 +0900

    Updated 7.py and requirements.txt

--- 7.py
+++ 7.py
@@ -36,49 +36,52 @@ try:
             builder=pyocr.builders.WordBoxBuilder(tesseract_layout=11)
         )
         tolerance = 100
-        reference_y = word_boxes[0].position[0][1]
-        crop_imgs = []
-        crop_dict = {"xmin": 0, "xmax": img_cv.shape[1], "ymin": None, "ymax": None}
+        if word_boxes:  # word_boxesが空でないことを確認
+            reference_y = word_boxes[0].position[0][1]
+            crop_imgs = []
+            crop_dict = {"xmin": 0, "xmax": img_cv.shape[1], "ymin": None, "ymax": None}
 
-        for i, box in enumerate(word_boxes):
-            x1, y1 = box.position[0]
-            x2, y2 = box.position[1]
-            if not (reference_y - tolerance <= y1 <= reference_y + tolerance):
-                reference_y = y1
-                crop_imgs.append(crop_dict)
-                crop_dict = {"xmin": 0, "xmax": img_cv.shape[1], "ymin": None, "ymax": None}
+            for i, box in enumerate(word_boxes):
+                x1, y1 = box.position[0]
+                x2, y2 = box.position[1]
+                if not (reference_y - tolerance <= y1 <= reference_y + tolerance):
+                    reference_y = y1
+                    crop_imgs.append(crop_dict)
+                    crop_dict = {"xmin": 0, "xmax": img_cv.shape[1], "ymin": None, "ymax": None}
 
-            if reference_y - tolerance <= y1 <= reference_y + tolerance:
-                crop_dict["ymin"] = y1 if crop_dict["ymin"] is None else min(crop_dict["ymin"], y1)
-                crop_dict["ymax"] = y2 if crop_dict["ymax"] is None else max(crop_dict["ymax"], y2)
+                if reference_y - tolerance <= y1 <= reference_y + tolerance:
+                    crop_dict["ymin"] = y1 if crop_dict["ymin"] is None else min(crop_dict["ymin"], y1)
+                    crop_dict["ymax"] = y2 if crop_dict["ymax"] is None else max(crop_dict["ymax"], y2)
 
-        crop_imgs.append(crop_dict)
+            crop_imgs.append(crop_dict)
 
-        # 切り抜いた画像をOCRで処理し、Wordファイルに出力
-        doc = Document()
-        paragraphs = [] # パラグラフを一度に追加するためのリスト
-        for i, crop_dict in enumerate(crop_imgs):
-            cropped_image = img_cv[crop_dict["ymin"]:crop_dict["ymax"], crop_dict["xmin"]:crop_dict["xmax"]]
-            if cropped_image.size > 0:  # 切り取られた画像が空でない場合のみ処理
-                image = Image.fromarray(cropped_image).convert("RGB")
-                generated_ids = model.generate(processor(image, return_tensors="pt").pixel_values)
-                S = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-                paragraphs.append(S)
+            # 切り抜いた画像をOCRで処理し、Wordファイルに出力
+            doc = Document()
+            paragraphs = [] # パラグラフを一度に追加するためのリスト
+            for i, crop_dict in enumerate(crop_imgs):
+                cropped_image = img_cv[crop_dict["ymin"]:crop_dict["ymax"], crop_dict["xmin"]:crop_dict["xmax"]]
+                if cropped_image.size > 0:  # 切り取られた画像が空でない場合のみ処理
+                    image = Image.fromarray(cropped_image).convert("RGB")
+                    generated_ids = model.generate(processor(image, return_tensors="pt").pixel_values)
+                    S = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+                    paragraphs.append(S)
 
-        for para in paragraphs:  # ここで一度にパラグラフを追加
-            doc.add_paragraph(para)
+            for para in paragraphs:  # ここで一度にパラグラフを追加
+                doc.add_paragraph(para)
 
-        # Wordファイルの保存
-        buffer = BytesIO()
-        doc.save(buffer)
-        buffer.seek(0)
+            # Wordファイルの保存
+            buffer = BytesIO()
+            doc.save(buffer)
+            buffer.seek(0)
 
-        # Wordファイルのダウンロード
-        st.download_button(
-            label="Download output.docx",
-            data=buffer,
-            file_name='output.docx',
-            mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-        )
+            # Wordファイルのダウンロード
+            st.download_button(
+                label="Download output.docx",
+                data=buffer,
+                file_name='output.docx',
+                mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+            )
+        else:
+            st.write("No text could be detected in the image.")
 except Exception as e:
     st.write(f"An error occurred: {str(e)}")
